4cc7f822585cbe4f44a50841a0798281123c55e1
Beck-Haywood <beck.haywood@students.makeschool.com>
2020-11-23 15:04:36 -0900

    Initial new channel

--- sockets/chat.js
+++ sockets/chat.js
@@ -27,6 +27,10 @@ module.exports = (io, socket, onlineUsers) => {
       delete onlineUsers[socket.username]
       io.emit('user has left', onlineUsers);
     });
+    // New Channel
+    socket.on('new channel', (newChannel) => {
+        console.log(newChannel);
+    })
 
 
   
